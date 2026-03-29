02af02eb9de97a47c2bc74c815817aa4eda871af
tducn-student <duc.nguyen240205@vnuk.edu.vn>
2026-03-29 20:02:37 +0700

    fix: resolve typescript unknown type in financeController

--- server/src/middleware/validateMiddleware.js
+++
@@ -1,9 +0,0 @@
-const validate = (schema) => (req, res, next) => {
-    const { error } = schema.validate(req.body);
-    if (error) {
-        return res.status(400).json({ success: false, message: error.details[0].message });
-    }
-    next();
-};
-
-module.exports = validate;
